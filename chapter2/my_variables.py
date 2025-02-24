6b05bee98b60f176c27ff91fd5dc854300dd7bcc
ethan6938 <en.hurwitz@gmail.com>
2025-02-24 14:05:22 +0200

    python chapter2

---
+++ chapter2/my_variables.py
@@ -0,0 +1,34 @@
+# name = "ethan"
+
+# surname = "hurwitz"
+
+# print(name + " " + surname)
+
+# my_sentence = "hay i am ethan nice to meet you."
+
+# print(my_sentence.title())
+
+# my_initials = f" {name} {surname}"
+
+# print(my_initials)
+
+# name = "ethan"
+# surname = "hurwitz"
+# age = "16"
+
+# my_fullname_age = f"{name} {surname} {age}"
+
+# print(my_fullname_age)
+
+# print("\ti like gaming and playing airsoft")
+
+# print("games:\ngta\nfifa\nMarval rivals")
+
+# name, surname, age = "ethan" , "hurwitz" , "24"
+
+# print(name)
+
+
+import this
+# The Zen of Python, by Tim Peters
+# Beautiful is better than ugly.
