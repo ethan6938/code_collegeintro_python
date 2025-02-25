b3a1f0744b138cdf91ba49ebb48e6d8c643fa301
ethan6938 <en.hurwitz@gmail.com>
2025-02-25 18:33:10 +0200

    python/chapter3

---
+++ chapter3/chapter3.py
@@ -0,0 +1,22 @@
+# my_list = ["aidan","cadee","courtney","lindo","phomello","ronny","sibu","tom","ulrich","mieke","asanda","marvelous","pierre","myself"]
+
+# my_list.append("david")
+
+# my_list.insert(0,"rayma")
+
+# del my_list[0]
+
+# my_list.sort(reverse=True)
+
+# print(my_list)
+
+
+
+
+the_world  = ["Portugal","Japan","Finland","spain","argentina "]
+
+print(the_world)
+print(sorted(the_world))
+print(the_world)
+print(sorted(the_world, reverse=True))
+print(the_world)
