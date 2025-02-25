2d72b77749bfad492b3857fb1e4c50cf4491852a
ethan6938 <en.hurwitz@gmail.com>
2025-02-25 18:33:29 +0200

    python/chapter4

---
+++ chapter4/chapter4.py
@@ -0,0 +1,12 @@
+favourite_moives = ["transformers","starwars","mazerunner"]
+for movie in favourite_moives:
+    proper_title = movie.title()
+    print(f"this is one of my favourite movies {proper_title}")
+
+numbers =["1","2","3"]
+
+my_tuple=("2","4","6")
+
+numbers.insert(0,"etha")
+
+print(numbers)
