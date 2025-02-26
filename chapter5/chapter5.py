f14b98b73d0220cd495de3e608cd42fff5a1f3db
ethan6938 <en.hurwitz@gmail.com>
2025-02-26 16:30:55 +0200

    chapter5

---
+++ chapter5/chapter5.py
@@ -0,0 +1,13 @@
+my_hobbies = ["airsoft","gaming","coding","soccer"]
+tester = False
+
+for hobbies in my_hobbies:
+    if hobbies == "gaming":
+        print("gaming is good")
+    else:
+        print("that is cool i guess.")
+
+if tester == True:
+    print("the value is true")
+else:
+    print("the value is false")
